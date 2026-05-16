b4829dee8bc844aff81a1d5c22022a8d471358e0
Mr Robot <robot@example.com>
2026-05-16 19:16:09 +0530

    feat: added useAuth hook for firebase auth state handling

---
+++ src/hooks/useAuth.js
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { doc, getDoc } from "firebase/firestore"
+import { auth, db } from "../firebase"
+
+
+export const useAuth = () => {
+    const [currentUser, setCurrentUser] = useState(null)
+    const [userProfile, setUserProfile] = useState(null)
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, async (user) => {
+            setCurrentUser(user)
+
+            if (user) {
+                const docRef = doc(db, "users", user.uid)
+                const docSnap = await getDoc(docRef)
+
+                if (docSnap.exists()) {
+                    setUserProfile(docSnap)
+                }
+                else {
+                    setUserProfile(null)
+                }
+            }
+            else {
+                setUserProfile(null)
+            }
+            setLoading(false)
+
+            return () => unsubscribe()
+        })
+    },[])
+    return {currentUser,userProfile,loading}
+}
