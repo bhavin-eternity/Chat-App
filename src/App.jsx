70909ae4724d5233446219add55ab5400515e825
Mr Robot <robot@example.com>
2026-05-16 21:22:05 +0530

    added browserrouter and routes in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,44 @@
-import { Button } from "@/components/ui/button"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+import { useAuth } from "./hooks/useAuth"
+import Login from './components/auth/Login'
+import UsernameSetup from "./components/auth/UsernameSetup"
+import Home from "./components/home/Home"
 
 function App() {
-  return (
-    <div className="flex items-center justify-center h-screen bg-slate-950">
-      <div className="text-center space-y-4">
-        <h1 className="text-3xl font-bold text-white">Vibe 💬</h1>
-        <Button>Let's go</Button>
-        <br></br>
-        <Button>Let's go</Button>
+  const { currentUser, userProfile, loading } = useAuth()
+
+  if (loading) {
+    return (
+      <div className="flex items-center justify-center h-screen bg-slate-950">
+        <div className="text-white text-lg animate-pulse">loading...</div>
       </div>
-    </div>
+    )
+  }
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            !currentUser ? <Login /> :
+              !userProfile?.username ? <UsernameSetup /> :
+                <Home />
+          }
+        />
+
+        <Route
+          path="/chat/:conversationId"
+          element={currentUser ? <Home /> : <Navigate to="/" />}
+        />
+
+        <Route
+          path="/profile/:username"
+          element={currentUser ? <Home /> : <Navigate to="/" />}
+        />
+
+
+      </Routes>
+    </BrowserRouter>
   )
 }
 
